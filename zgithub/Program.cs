347f2f246a8bcbf7c2631f5a344a508fa9eee184
ggeryz <cobra166@abv.bg>
2016-11-23 16:06:02 +0200

    my first commit to github 23.11.2016

---
+++ zgithub/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zgithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("After midnight the clowns aren't funny." );
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
